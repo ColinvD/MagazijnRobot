3bfea43d16895b34d06d8b8d7c9f1fa4a0b43139
ColinvD <vanDongen.colin@gmail.com>
2024-04-15 15:25:33 +0200

    ProjectBase

---
+++ HMI/MagazijnRobot/src/HMIScreen.java
@@ -0,0 +1,5 @@
+public class HMIScreen {
+    public HMIScreen(){
+
+    }
+}
---
+++ HMI/MagazijnRobot/src/Main.java
@@ -0,0 +1,5 @@
+public class Main {
+    public static void main(String[] args) {
+        HMIScreen hmiScreen = new HMIScreen();
+    }
+}
