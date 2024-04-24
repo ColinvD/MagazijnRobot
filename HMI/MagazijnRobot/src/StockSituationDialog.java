d2c4f84c5f49794a1678f4ba7233ced1490edcc4
milan-bakker <milan@bakkerisolatie.nl>
2024-04-24 14:15:36 +0200

    Done
    everything is done

--- HMI/MagazijnRobot/src/StockSituationDialog.java
+++ HMI/MagazijnRobot/src/StockSituationDialog.java
@@ -1,7 +1,11 @@
 import javax.swing.*;
+import javax.xml.crypto.Data;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.EventListener;
@@ -15,11 +19,13 @@ public class StockSituationDialog extends JDialog implements ActionListener {
     private JCheckBox jcEmpty, jcFull;
     private JComboBox jcbProduct;
     private ArrayList<JButton> gridButton = new ArrayList<>();
+    private String selectedStockLocation;
 
     public StockSituationDialog() {
         setModal(true);
         setTitle("Voorraadsituatie");
         setSize(new Dimension(500, 500));
+        setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
 
 //        top buttons
         buttonPanel = new JPanel();
@@ -81,9 +87,29 @@ public class StockSituationDialog extends JDialog implements ActionListener {
         valuePanel.setBorder(BorderFactory.createLineBorder(Color.black));
         jcEmpty = new JCheckBox("Leeg");
         jcFull = new JCheckBox("Gevuld met item:");
+        jcEmpty.addActionListener(this);
+        jcFull.addActionListener(this);
 
+        String boxValue[] = new String[0];
+        int count = 1;
+        try {
+            Database database = new Database();
+            database.databaseConnect();
+            ResultSet resultCount = database.select("Select count(*) FROM stockitems");
+            resultCount.next();
+            boxValue = new String[resultCount.getInt("count(*)") + 1];
+            boxValue[0] = "";
+            ResultSet result = database.select("Select * FROM stockitems");
+            while (result.next()) {
+                boxValue[count] = result.getString(2);
+                count++;
+            }
+            result.close();
+            database.close();
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
 
-        String boxValue[] = {"prod1", "tew2", "efa3"};
         jcbProduct = new JComboBox(boxValue);
 
         valuePanel.add(jcFull);
@@ -108,17 +134,91 @@ public class StockSituationDialog extends JDialog implements ActionListener {
     @Override
     public void actionPerformed(ActionEvent e) {
         if (e.getSource() == confirmButton) {
-
+            changeProductStock();
+            return;
         }
-        if (e.getSource() == cancelButton || e.getSource() == confirmButton) {
+        if (e.getSource() == cancelButton) {
             setVisible(false);
             return;
         }
+        if (e.getSource() == jcEmpty) {
+            jcFull.setSelected(false);
+        }
+        if (e.getSource() == jcFull) {
+            jcEmpty.setSelected(false);
+        }
         for (JButton jButton : gridButton) {
             if (e.getSource() == jButton) {
+                selectedStockLocation = jButton.getText();
+                String boxValueSelected = getProductFromStock(jButton);
+                if (boxValueSelected == null || boxValueSelected.equals("")) {
+                    jcEmpty.setSelected(true);
+                    jcFull.setSelected(false);
+                } else {
+                    jcFull.setSelected(true);
+                    jcEmpty.setSelected(false);
+                }
+                System.out.println(boxValueSelected);
+                jcbProduct.setSelectedItem(boxValueSelected);
                 System.out.println(jButton.getText());
                 return;
             }
         }
+    }
+
+    private void changeProductStock() {
+        try {
+            Database database = new Database();
+            database.databaseConnect();
+            int resultUpdate = 0;
+            String resultString = "";
+            boolean newProduct = false;
+            if (jcFull.isSelected() && !jcEmpty.isSelected()) {
+                database.update("UPDATE stockitems SET StockLocation = NULL WHERE StockLocation = ?", selectedStockLocation);
+                if(!jcbProduct.getSelectedItem().toString().isEmpty()) {
+                    resultString = database.selectFirst("SELECT StockLocation FROM stockitems WHERE StockItemName = ?", jcbProduct.getSelectedItem().toString());
+                    resultUpdate = database.update("UPDATE stockitems SET StockLocation = ? WHERE StockItemName = ?", selectedStockLocation, jcbProduct.getSelectedItem().toString());
+                    newProduct = true;
+                }
+            } else if (jcEmpty.isSelected() && !jcFull.isSelected()){
+                database.update("UPDATE stockitems SET StockLocation = NULL WHERE StockLocation = ?", selectedStockLocation);
+            } else {
+                JOptionPane.showMessageDialog(null, "Er zijn geen checkboxes geselecteerd", "error", JOptionPane.ERROR_MESSAGE);
+            }
+            if (newProduct) {
+                if (resultUpdate > 0) {
+                    if (resultString == null || resultString.isEmpty()) {
+                        JOptionPane.showMessageDialog(null, "Het product: " + jcbProduct.getSelectedItem().toString() + " is verplaatst  naar schap: " + selectedStockLocation + ".", "voorraad locatie aangepast", JOptionPane.INFORMATION_MESSAGE);
+                    } else {
+                        JOptionPane.showMessageDialog(null, "Het product: " + jcbProduct.getSelectedItem().toString() + " is verplaatst van schap: " + resultString + " naar schap: " + selectedStockLocation + ".", "voorraad locatie aangepast", JOptionPane.INFORMATION_MESSAGE);
+                    }
+                } else {
+                    JOptionPane.showMessageDialog(null, "Er is iets fout gegaan", "error", JOptionPane.ERROR_MESSAGE);
+                }
+            } else {
+                JOptionPane.showMessageDialog(null, "Het schap: " + selectedStockLocation + " is geleegd.", "voorraad geleegd", JOptionPane.INFORMATION_MESSAGE);
+            }
+            database.close();
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+
+
+    }
+
+    private String getProductFromStock(JButton stockLocation) {
+        try {
+            Database database = new Database();
+            database.databaseConnect();
+            ResultSet result = database.select("Select stockItemName From StockItems WHERE StockLocation = ?", stockLocation.getText());
+            result.next();
+            String boxValueSelected = result.getString(1);
+            result.close();
+            database.close();
+            return boxValueSelected;
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        return null;
     }
 }
