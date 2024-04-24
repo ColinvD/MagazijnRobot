73f019ae92e6f0e299e7b6d8bd8c069ff3fdb73d
Kars082004 <163519527+Kars082004@users.noreply.github.com>
2024-04-24 14:11:08 +0200

    Robot 1 encoder

--- Robot/Robot1/Robot1/Robot1.ino
+++ Robot/Robot1/Robot1/Robot1.ino
@@ -2,29 +2,76 @@ const int directionPinUP = 12;
 const int pwmPinUP = 3;
 const int brakeUP = 9;
 
-const int power = 200;
+// const int IsLeft = 10;
+// const int IsLeft
+const int encoderZ = 5;
 
+const int power = 135;
+unsigned long LastTime;
+int waitTime = 2000;
+int currentState = 4;
 void setup() {
   // put your setup code here, to run once:
   pinMode(directionPinUP, OUTPUT);
   pinMode(pwmPinUP, OUTPUT);
   pinMode(brakeUP, OUTPUT);
+  //pinMode(IsLeft,INPUT);
+  pinMode(encoderZ, INPUT);
+  Serial.begin(9600);
 }
 
 void loop() {
+  // int sensor_value = digitalRead(IsLeft);
+  int encoder_variable = digitalRead(encoderZ);
+  //Serial.println(encoder_variable);
+  // if(sensor_value == HIGH){
+  //   digitalWrite(directionPinUP, LOW);
+  //   analogWrite(pwmPinUP, power);
+  //   digitalWrite(brakeUP, LOW);
+  //   Serial.println("aan");
+  // }
+  // else {
+  //   analogWrite(pwmPinUP, 0);
+  //   digitalWrite(brakeUP, HIGH);
+  //   Serial.println("dichtbij");
+  // }
+
+  if (millis() > LastTime + waitTime) {
+    LastTime = millis();
+    Something();
+  }
+  if(encoder_variable == HIGH){
+    Serial.println("Rotation");
+  }
+  //Serial.println(encoder_variable);
+}
+
+void Something() {
   // put your main code here, to run repeatedly:
-  delay(2000);
-  digitalWrite(directionPinUP, HIGH);
-  analogWrite(pwmPinUP, power);
-  digitalWrite(brakeUP, LOW);
-  delay(2000);
-  analogWrite(pwmPinUP, 0);
-  digitalWrite(brakeUP, HIGH);
-  delay(2000);
-  digitalWrite(directionPinUP, LOW);
-  analogWrite(pwmPinUP, power);
-  digitalWrite(brakeUP, LOW);
-  delay(6000);
-  analogWrite(pwmPinUP, 0);
-  digitalWrite(brakeUP, HIGH);
+  switch (currentState) {
+    case 0:
+      digitalWrite(directionPinUP, HIGH);
+      analogWrite(pwmPinUP, power);
+      digitalWrite(brakeUP, LOW);
+      currentState = 1;
+      break;
+    case 1:
+      analogWrite(pwmPinUP, 0);
+      digitalWrite(brakeUP, HIGH);
+      currentState = 2;
+      break;
+    case 2:
+      digitalWrite(directionPinUP, LOW);
+      analogWrite(pwmPinUP, power);
+      digitalWrite(brakeUP, LOW);
+      currentState = 3;
+      break;
+    case 3:
+      analogWrite(pwmPinUP, 0);
+      digitalWrite(brakeUP, HIGH);
+      currentState = 0;
+      break;
+    default:
+      currentState = 0;
+  }
 }
