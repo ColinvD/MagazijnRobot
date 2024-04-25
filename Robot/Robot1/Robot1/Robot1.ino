a93a38c8598eff36ebbd1b4ceb3030e6477f906f
Kars082004 <163519527+Kars082004@users.noreply.github.com>
2024-04-25 16:34:35 +0200

    infrared sensor z-as

--- Robot/Robot1/Robot1/Robot1.ino
+++ Robot/Robot1/Robot1/Robot1.ino
@@ -2,27 +2,58 @@ const int directionPinUP = 12;
 const int pwmPinUP = 3;
 const int brakeUP = 9;
 
-// const int IsLeft = 10;
-// const int IsLeft
-const int encoderZ = 5;
+const int distance = A2;
+//const int encoderZ = 5;
 
 const int power = 135;
 unsigned long LastTime;
 int waitTime = 2000;
 int currentState = 4;
+
+
+//Button var
+const int uit = 7;
+const int in = 10;
+bool pressedOut = false;
+bool isReleasedOut = true;
+bool pressedIn = false;
+bool isReleasedIn = true;
 void setup() {
   // put your setup code here, to run once:
   pinMode(directionPinUP, OUTPUT);
   pinMode(pwmPinUP, OUTPUT);
   pinMode(brakeUP, OUTPUT);
-  //pinMode(IsLeft,INPUT);
-  pinMode(encoderZ, INPUT);
+  //pinMode(encoderZ, INPUT);
+  pinMode(uit, INPUT);
+  pinMode(in, INPUT);
+  pinMode(distance,INPUT);
   Serial.begin(9600);
 }
 
 void loop() {
+  Distance();
+  pressedOut = digitalRead(uit);
+  pressedIn = digitalRead(in);
+  //Serial.println(pressedOut);
+  if (pressedOut && isReleasedOut) {
+    isReleasedOut = false;
+    GoOut();
+   // Serial.println("OUT!!!!!!");
+  } else if (!pressedOut && !isReleasedOut) {
+    isReleasedOut = true;
+    Stop();
+  }
+  if (pressedIn && isReleasedIn) {
+    isReleasedIn = false;
+    GoIn();
+    //Serial.println("In!!!!!!");
+  } else if (!pressedIn && !isReleasedIn) {
+    isReleasedIn = true;
+    Stop();
+  }
+
   // int sensor_value = digitalRead(IsLeft);
-  int encoder_variable = digitalRead(encoderZ);
+  //int encoder_variable = digitalRead(encoderZ);
   //Serial.println(encoder_variable);
   // if(sensor_value == HIGH){
   //   digitalWrite(directionPinUP, LOW);
@@ -36,42 +67,34 @@ void loop() {
   //   Serial.println("dichtbij");
   // }
 
-  if (millis() > LastTime + waitTime) {
-    LastTime = millis();
-    Something();
-  }
-  if(encoder_variable == HIGH){
-    Serial.println("Rotation");
-  }
+  // if (millis() > LastTime + waitTime) {
+  //   LastTime = millis();
+  //   Something();
+  // }
+  // if(encoder_variable == HIGH){
+  //   Serial.println("Rotation");
+  // }
   //Serial.println(encoder_variable);
 }
 
-void Something() {
-  // put your main code here, to run repeatedly:
-  switch (currentState) {
-    case 0:
-      digitalWrite(directionPinUP, HIGH);
-      analogWrite(pwmPinUP, power);
-      digitalWrite(brakeUP, LOW);
-      currentState = 1;
-      break;
-    case 1:
+void GoIn() {
+  digitalWrite(directionPinUP, HIGH);
+  analogWrite(pwmPinUP, power);
+  digitalWrite(brakeUP, LOW);
+}
+
+void GoOut() {
+  digitalWrite(directionPinUP, LOW);
+  analogWrite(pwmPinUP, power);
+  digitalWrite(brakeUP, LOW);
+}
+void Stop(){
       analogWrite(pwmPinUP, 0);
       digitalWrite(brakeUP, HIGH);
-      currentState = 2;
-      break;
-    case 2:
-      digitalWrite(directionPinUP, LOW);
-      analogWrite(pwmPinUP, power);
-      digitalWrite(brakeUP, LOW);
-      currentState = 3;
-      break;
-    case 3:
-      analogWrite(pwmPinUP, 0);
-      digitalWrite(brakeUP, HIGH);
-      currentState = 0;
-      break;
-    default:
-      currentState = 0;
-  }
+}
+
+void Distance(){
+  float volts = analogRead(distance)*0.0048828125;  // value from sensor * (5/1024)
+  int distanceRobot = 13*pow(volts, -1); // worked out from datasheet graph
+  Serial.println(distanceRobot);
 }
