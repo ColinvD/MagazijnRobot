61fe7a99b70855e83ddb9f0fc3e5420dc3f103f5
Kars082004 <163519527+Kars082004@users.noreply.github.com>
2024-05-14 11:53:15 +0200

    Noodstop toegevoegd compleet

--- HMI/MagazijnRobot/src/ButtonPanel.java
+++ HMI/MagazijnRobot/src/ButtonPanel.java
@@ -11,7 +11,7 @@ public class ButtonPanel extends JPanel implements ActionListener {
     private OrderPanel orderPanel;
 
     private boolean emergencyStatus;
-
+    SerialCommunicator serialCommunicator = new SerialCommunicator("COM4",500000);
     private JButton emergencyStop;
     public ButtonPanel(OrderPanel orderPanel){
         setPreferredSize(new Dimension(350,200));
@@ -44,10 +44,8 @@ public class ButtonPanel extends JPanel implements ActionListener {
             }
         }
         if (e.getSource()== emergencyStop){
-            // SerialCommunicator serialCommunicator = new SerialCommunicator("COM4",500000);
+
             if (!emergencyStatus) {
-                //try {
-              //      serialCommunicator.sendMessageToArduino("STOP");
                 UIManager.put("OptionPane.noButtonText", "Nee");
                 UIManager.put("OptionPane.yesButtonText", "Ja");
                 int choice = JOptionPane.showConfirmDialog(null, "Wil je de noodstop vergrendelen?",
@@ -56,13 +54,15 @@ public class ButtonPanel extends JPanel implements ActionListener {
                     JOptionPane.showMessageDialog(null, "Noodstop vergrendeld.");
                     emergencyStatus = true;
                     emergencyStop.setText("Schakel noodstop uit");
+                    try {
+                        serialCommunicator.sendMessageToArduino("STOP");
+                    } catch (IOException ex) {
+                        throw new RuntimeException(ex);
+                    }
                 } else if (choice == JOptionPane.NO_OPTION) {
                     JOptionPane.showMessageDialog(null, "Noodstop niet vergrendeld.");
                 }
-               // jPopupMenu.setLayout(new FlowLayout());
-             //   } catch (IOException ex) {
-                 //   throw new RuntimeException(ex);
-               // }
+
             }
             else{
                 UIManager.put("OptionPane.yesButtonText", "Ja");
@@ -74,7 +74,11 @@ public class ButtonPanel extends JPanel implements ActionListener {
                     JOptionPane.showMessageDialog(null, "Noodstop ontgrendeld.");
                     emergencyStop.setText("Schakel noodstop in");
                     emergencyStatus = false;
-                    //      serialCommunicator.sendMessageToArduino("Unlock");
+                    try {
+                        serialCommunicator.sendMessageToArduino("Unlock");
+                    } catch (IOException ex) {
+                        throw new RuntimeException(ex);
+                    }
                 } else if (choice == JOptionPane.NO_OPTION) {
                     JOptionPane.showMessageDialog(null, "Noodstop niet ontgrendeldt.");
                 }
