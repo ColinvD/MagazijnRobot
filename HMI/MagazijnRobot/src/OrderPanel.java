22e321d35a6a9608dd5e20a2b75a2ed7c57cd83c
Kars082004 <163519527+Kars082004@users.noreply.github.com>
2024-05-27 12:23:07 +0200

    test

--- HMI/MagazijnRobot/src/OrderPanel.java
+++ HMI/MagazijnRobot/src/OrderPanel.java
@@ -12,6 +12,8 @@ import java.util.ArrayList;
 import java.util.Arrays;
 
 public class OrderPanel extends JPanel implements ActionListener, Listener {
+    ArrayList<String> orderlist = new ArrayList<>();
+    private int duplicates = 0;
     private int k = 0;
     private int m = 0;
 
@@ -85,12 +87,10 @@ public class OrderPanel extends JPanel implements ActionListener, Listener {
                     products.add(locatie);
                 }
             }
-            Boxes = BBP.firstFitDec(products, products.size(), 10);
+            Boxes = BBP.firstFitDec(products, products.size(), 15);
 
             //This only shows the first box of the order
-
             ArrayList<Locatie> box = new ArrayList<>(Boxes.getFirst());
-
             ArrayList<Locatie> route = TSP.getRoute(box);
             StatusPanel.displayRoute(route);
             SchapPanel.drawRoute(route);
@@ -103,6 +103,19 @@ public class OrderPanel extends JPanel implements ActionListener, Listener {
                 }
                 products1.add(doos);
                 orderItemsPanel.add(doos);
+                ArrayList<Locatie> box1 = new ArrayList<>(Boxes.get(i));
+                ArrayList<Locatie> routes = TSP.getRoute(box1);
+                // System.out.println(Arrays.toString(box));
+                ArrayList<Locatie> locaties = new ArrayList<>(box1.size());
+                for (int k = 0; k < routes.size();k++){
+                    for (int l = 0; l < box1.size(); l++) {
+                        if(routes.get(k).getLocation().equals(box1.get(l).getLocation())){
+                            locaties.add(box1.get(l));
+                            break;
+                        }
+                    }
+                }
+                Boxes.add(i, locaties);
                 for (int j = 0; j < Boxes.get(i).size(); j++) {
                     String location = Boxes.get(i).get(j).getLocation();
                     ResultSet stockitem = database.getStockitem(location);
@@ -120,7 +133,7 @@ public class OrderPanel extends JPanel implements ActionListener, Listener {
 //                     }
 //                     JLabel product = new JLabel(orderItems.getInt("StockItemID") + ". " + orderItems.getString("StockItemName")  + ".  "+Stocklocation);
 //                     product.setPreferredSize(new Dimension(280, 12));
-                    orderItemsPanel.add(product);
+
                 }
             }
         }
@@ -131,45 +144,77 @@ public class OrderPanel extends JPanel implements ActionListener, Listener {
         this.updateUI();
     }
 
-    public void changeOrderColor() throws SQLException {
-        int l = 0;
-        orderItemsPanel.removeAll();
-        sizebox = Boxes.get(m).size();
-        if (k == sizebox) {
-            m++;
-            k = 0;
-            sizebox = Boxes.get(m).size();
-        }
-        ArrayList<Locatie> locaties = new ArrayList<>();
-        counterChangeAmountColor = 0;
-        for (int i = 0; i < sizebox; i++) {
-            locaties.add(Boxes.get(m).get(i));
-        }
-        // System.out.println(Arrays.toString(box));
-        ArrayList<Locatie> route = TSP.getRoute(locaties);
-        System.out.println(route);
-        database.updatepicked(route.get(k).getOrderlineID());
-        database.updateOrderlineAfterOrder(route.get(k).getOrderlineID());
-        int doosCount = 0;
-        if (k == 1 && sizebox > 2) {
-            l = 1; // idk why this but it works for now. (get back to it later)
-        }
-        for (JLabel label : products1) {
-            if (!label.getText().contains("Doos")) {
-                if (label.getText().substring(0, 2).equals(route.get(k).getLocation()) && m + 1 == doosCount) {
-                    if (l <= k) {
-                        label.setForeground(Color.GREEN);
-                        l++;
-                    }
-                }
-            } else {
-                doosCount++;
-            }
-            orderItemsPanel.add(label);
-        }
-        k++;
-        updateUI();
-    }
+//    public void changeOrderColor() throws SQLException {
+//        orderItemsPanel.removeAll();
+//        sizebox = Boxes.get(m).size();
+//        if (k == sizebox) {
+//            m++;
+//            k = 0;
+//            sizebox = Boxes.get(m).size();
+//            duplicates = 0;
+//            orderlist = new ArrayList<>();
+//        }
+//        ArrayList<Locatie> locaties = new ArrayList<>();
+//        for (int i = 0; i < sizebox; i++) {
+//            locaties.add(Boxes.get(m).get(i));
+//        }
+//        // System.out.println(Arrays.toString(box));
+//        ArrayList<Locatie> route = TSP.getRoute(locaties);
+//        System.out.println(route);
+//        database.updatepicked(route.get(k).getOrderlineID());
+//        database.updateOrderlineAfterOrder(route.get(k).getOrderlineID());
+//        int doosCount = 0;
+//        //System.out.println(l);
+//
+////        if (k >= 1 && sizebox > k+1) {
+////            l += k; //tf is dit en waarom werkt dit bij 3 en soms 4??????
+////            System.out.println(k+" "+l);
+////        }
+////
+////        if (k >= 2 && sizebox == 3) {
+////            l--; // idk why this but it works for now. (get back to it later)
+////            System.out.println(k + " " + l);
+////        }
+//       // System.out.println(sizebox);
+//        for (JLabel label : products1) {
+//            if (!label.getText().contains("Doos")) {
+//                if (label.getText().substring(0, 2).equals(route.get(k).getLocation()) && m + 1 == doosCount) {
+//                    if (orderlist.contains(label.getText())) {
+//
+////                            if () {
+////                                label.setForeground(Color.GREEN);
+////                            }
+//                        System.out.println(duplicates);
+//                        System.out.println(k);
+////                        if (sizebox == 2){
+////                            System.out.println(sizebox);
+////                            label.setForeground(Color.GREEN);
+////                       {
+////                        }
+//                     } else {
+//                     //   System.out.println(duplicates);
+//                        label.setForeground(Color.GREEN);
+//                        duplicates = 0;
+//                    }
+////                    if (l <= k) {
+////                        label.setForeground(Color.GREEN);
+////                        l++;
+////                    }
+//                    orderlist.add(label.getText());
+////                    for (String a : orderlist){
+////                        System.out.println(a);
+////                    }
+//                }
+//            } else {
+//                doosCount++;
+////                duplicates = 0;
+////                orderlist= new ArrayList<>();
+//            }
+//            orderItemsPanel.add(label);
+//        }
+//        k++;
+//        updateUI();
+//    }
 
     @Override
     public void actionPerformed(ActionEvent e) {
@@ -181,7 +226,7 @@ public class OrderPanel extends JPanel implements ActionListener, Listener {
 //                    }
 //            try {
 //                counterMaxAmountColor++;
-//                changeOrderColor();
+//               // changeOrderColor();
 //            } catch (SQLException ex) {
 //                throw new RuntimeException(ex);
 //            }
@@ -195,7 +240,7 @@ public class OrderPanel extends JPanel implements ActionListener, Listener {
         if (message.equals("Out")) {
             System.out.println("out");
             counterMaxAmountColor++;
-            changeOrderColor();
+           // changeOrderColor();
         }
         if (message.equals("Complete")) {
             System.out.println("complete");
